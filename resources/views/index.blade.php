932c3d77eee58085cb0a6334196e80c4bc7fd910
areebanaz-67 <areebanazch512@gmail.com>
2024-05-12 12:56:43 +0500

    added laravel files

---
+++ resources/views/index.blade.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parishay Crochet Collection</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+    <header>
+        <div class="navbar" id="navbar1">
+            <div class="nav-logo border">
+                <a href="index.html" class="logo">
+                    <img src="pc-monogram-logo-vector-33144481-removebg-preview.png" style="height: 75px; width: 100%;background-size:contain; background-repeat: no-repeat;">
+                </a>
+            </div>
+            <div class="brandname">
+                Parishay Crochet Collection
+            </div>
+            <a href="#product" class="nav-options">Categories</a>
+            <a href="Search.html" class="nav-options">Search</a>
+            <a href="Cart.html" class="nav-options" ">Cart</a>
+            <a href="About.html" class="nav-options">About</a>
+            <a href="Signup.html" class="nav-options">Sign up</a>
+            <a href="reterive.html" class="nav-options">Reterive Data</a>
+            <a href="login.html" class="nav-options">Login</a> 
+        </div>
+        <hr>
+    </header>
+    <div class="hero-section">
+        <!-- <div class="hero-msg">
+        </div> -->
+        <div id="prev-btn">
+            <i class="fa-solid fa-angle-up fa-rotate-270"></i>
+        </div>
+        <div class ="sliderimages">
+            <img src="" style="height: 600px; width: 100%;">
+        </div>
+        <div id="next-btn">
+            <i class="fa-solid fa-angle-up fa-rotate-90"></i>
+        </div>
+        <!-- <h3 class="hero-text">Crochet New Design for Men, Woman & children</h3> -->
+    </div>
+    <div class="category-heading" id="product">
+        Product Categories
+        <hr style="margin-top: 10px;">
+    </div>
+    <div class="category-box">
+        <div class="box1 box">
+            <div class="box-content">
+                <h2>Jewelry</h2>
+                <div class="box-img" style="background-image: url('jewelry.jpeg')">
+    
+                </div>
+                <a href="jewelry.html">See more</a>
+    
+            </div>
+        </div>
+        <div class="box2 box">
+            <div class="box-content">
+                <h2>Stylish Bags</h2>
+                <div class="box-img" style="background-image: url('bags.jpeg')">
+    
+                </div>
+                <a href="">See more</a>
+    
+            </div>
+        </div>
+        <div class="box3 box">
+            <div class="box-content">
+                <h2>Woman Clothes</h2>
+                <div class="box-img" style="background-image: url('clothes.jpeg')">
+    
+                </div>
+                <p>See more</p>
+    
+            </div>
+        </div>
+        <div class="box4 box">
+            <div class="box-content">
+                <h2>Home Decor</h2>
+                <div class="box-img" style="background-image: url('decor.png')">
+    
+                </div>
+                <p>See more</p>
+    
+            </div>
+        </div>
+    </div>
+    <div class="category-heading">
+        Featured Collection
+        <hr style="margin-top: 10px;">
+
+    </div>
+    <div class="category-box">
+        <div class="box1 box">
+            <div class="box-content">
+                <h2>Crohoni Bag</h2>
+                <div class="box-img" style="background-image: url('crohoni\ bag.webp')">
+    
+                </div>
+                <p style="padding:10px;">See more</p>
+    
+            </div>
+        </div>
+        <div class="box2 box">
+            <div class="box-content">
+                <h2>Summer Collection</h2>
+                <div class="box-img" style="background-image: url('summer\ collection.webp')">
+    
+                </div>
+                <p style="padding:10px;">See more</p>
+    
+            </div>
+        </div>
+        <div class="box3 box">
+            <div class="box-content">
+                <h2>Babies Playset</h2>
+                <div class="box-img" style="background-image: url('Babies\ playset.png')">
+    
+                </div>
+                <p style="padding:10px;">See more</p>
+    
+            </div>
+        </div>
+        <div class="box4 box">
+            <div class="box-content">
+                <h2>Linen linen-bracelet</h2>
+                <div class="box-img" style="background-image: url('linen-bracelet-2.jpg')">
+    
+                </div>
+                <p style="padding:10px;">See more</p>
+    
+            </div>
+        </div>
+    </div>
+    
+    <footer>
+        <div class="foot-panel">
+           <a href="#navbar1" style="text-decoration: none;"> Back TO TOP</a> 
+        </div>
+        <div class="foot-panel2">
+            <ul>
+                <p>General</p>
+                <a>About Us</a>
+                <a>Privacy Policy</a>
+                <a>Terms & Conditions</a>
+                <a>Modes of Payment</a>
+            </ul>
+            <ul>
+                <p>Top Categories</p>
+                <a>Bags</a>
+                <a>Jewelry</a>
+                <a>Women Clothes</a>
+                <a>Infant Accessories</a>
+                <a>Mens Sweater</a>
+            </ul>
+            <ul>
+                <p>Customer Care</p>
+                <a>Delivery & Returns</a>
+                <a>Contact Us</a>
+                <a>Customer Support</a>
+            </ul>
+        </div>
+        <div class="foot-panel3">
+            <div class="logo">
+
+            </div>
+        </div>
+        <div class="foot-panel4">
+            <div class="pages">
+                <a>Conditions of use</a>
+                <a>Privacy Notices</a>
+                <a>Yours add Privacy Policies</a>
+            </div>
+            <div class="copyright">
+                Parishaycrochetcollection.com, Inc. or its affiliates
+            </div>
+        </div>
+
+    </footer>
+    <script src="script.js" ></script>
+</body>
+</html>
